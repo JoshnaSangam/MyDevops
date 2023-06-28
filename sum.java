c38d5d23a64ee496df0bad002cbec7bfef2fb141
JoshnaSangam <sangamjoshnaaug11@gmail.com>
2023-06-28 11:36:04 +0530

    this is my  day2 commit

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("entyer any 2 nos.:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("sum is:"+(a+b));
+    }
+}
